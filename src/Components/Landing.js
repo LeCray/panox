f539407134c7e9b3c027a224432d27a6f4346a60
LeCray <mistercryptogrid@gmail.com>
2018-07-19 23:12:24 +0200

    Added the logos

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -37,19 +37,53 @@ export default class Landing extends Component {
             <div>              
                 <link href="https://fonts.googleapis.com/css?family=Quicksand:500" rel="stylesheet"/>			
                 <div style={{overflow: "hidden", fontFamily: "Quicksand", paddingTop: 50}}> 
-                    <div class="landing" style={{height: "100vh"}}>                               
-                        <div style={{marginTop: "17%", marginLeft: "20%"}}>
+                    <div class="landing" style={{height: "100vh"}}>
+                        <div style={{paddingTop: "17%", paddingLeft: "20%"}}>
                             <h2 style={{color: "#ea804a"}}>PANOX ELECTRONICS</h2>
                             <h5>Take your studies further</h5>
                         </div>
                     </div>
 
-                    <div class="container" style={{height: 500}}>
+                    <div class="container" style={{height: 2000, paddingTop: 100}}>
+                        <h2>Choose your brand</h2>
+                        <h5>We offer only the best to help define your image</h5>
+                        <hr/>
+                        <div class="row" style={{marginTop: 100, height: 200}}>
+                            <div class="col-lg-4" style={{textAlign: "center"}}>                                
+                                <img style={{height: 150}} src={require("../Resources/hp.png")} /> 
+                            </div>
+                            <div class="col-lg-4" style={{textAlign: "center"}}>
+                                <img style={{height: 150}} src={require("../Resources/apple.png")} class="img-fluid"/> 
+                            </div>
+                            <div class="col-lg-4" style={{textAlign: "center"}}>
+                                <img style={{height: 150}} src={require("../Resources/acer.png")} class="img-fluid"/> 
+                            </div>
+                        </div>
+
+                        <div class="row" style={{marginTop: 50}}>
+                            <div class="col-lg-4" style={{textAlign: "center"}}>                                
+                                <img style={{height: 150}} src={require("../Resources/asus.png")} /> 
+                            </div>
+                            <div class="col-lg-4" style={{textAlign: "center"}}>
+                                <img style={{height: 150}} src={require("../Resources/lg.png")} class="img-fluid"/> 
+                            </div>
+                            <div class="col-lg-4" style={{textAlign: "center"}}>
+                                <img style={{height: 150}} src={require("../Resources/samsung.png")} class="img-fluid"/> 
+                            </div>
+                        </div>
+
+                        <div class="row" style={{marginTop: 50}}>
+                            <div class="col-lg-4" style={{textAlign: "center"}}>                                
+                                <img style={{height: 150}} src={require("../Resources/dell.png")} /> 
+                            </div>
+                            <div class="col-lg-4" style={{textAlign: "center"}}>
+                                <img style={{height: 150}} src={require("../Resources/lenovo.png")} class="img-fluid"/> 
+                            </div>
+                        </div>
                     </div>
                         
             </div>
-                    }
-                    }
+              
         </div>
 		)
 	}
